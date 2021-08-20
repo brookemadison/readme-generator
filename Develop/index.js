0293bc5ceb87ded1c157ba5497622856dce52b42
brookemadison <brookeperry611@gmail.com>
2021-08-20 18:33:18 -0400

    included packages needed for this application

--- Develop/index.js
+++ Develop/index.js
@@ -5,7 +5,16 @@ const generatePage = require('./src/page-template');
 
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "What is the title fo your repository?",
+        name: "title"
+    },
+    {
+        
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
